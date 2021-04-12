76cce4c4d76bbef16ef6d3389490b64d9fd22cd7
Tom Knight <tomknight.dev@gmail.com>
2021-04-12 16:28:00 +0100

    Added vpn and spice scripts for macos

---
+++ SiconVPN.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo sstpc --log-stderr --log-level 4 --cert-warn --tls-ext --user=sicon\\tom.knight --password=S1c0n0420\!\# ras.sicon.co.uk usepeerdns require-mschap-v2 noauth noipdefault defaultroute require-mppe refuse-chap refuse-mschap noccp refuse-eap refuse-pap
